b1ff9b141e2b168d5a82bc642b066daf8f235927
mekya <ahmetmermerkaya@gmail.com>
2017-11-03 19:36:27 +0300

    Logout if user is not authenticated or server returns http error

--- src/app/rest/auth.service.ts
+++ src/app/rest/auth.service.ts
@@ -6,32 +6,37 @@ import { CanActivate, Router } from '@angular/router';
 
 export class User {
 
-  public newPassword:string;
-  public fullName:string;
+  public newPassword: string;
+  public fullName: string;
 
-  constructor(public email:string,public password:string) {}
+  constructor(public email: string, public password: string) { }
 }
 
 @Injectable()
-export class AuthService implements CanActivate{
+export class AuthService implements CanActivate {
 
-  private isAuthenticated:boolean = true;
+  private isAuthenticated: boolean = true;
 
-  constructor(private http:HttpClient, private router:Router) { }
+  constructor(private http: HttpClient, private router: Router) {
+    setInterval(() => {
+      this.checkServerIsAuthenticated();
+    }, 10000);
+
+  }
 
   test(): string {
     return 'working';
   }
 
-  login(email:string, password:string): Observable<Object>  {
+  login(email: string, password: string): Observable<Object> {
 
-    let user = new User (email, password);
+    let user = new User(email, password);
 
     return this.http.post(REST_SERVICE_ROOT + "/authenticateUser", user);
   }
 
-  changeUserPassword(email:string, password:string, newPassword:string): Observable<Object> {
-    let user = new User (email, password);
+  changeUserPassword(email: string, password: string, newPassword: string): Observable<Object> {
+    let user = new User(email, password);
     user.newPassword = newPassword;
     return this.http.post(REST_SERVICE_ROOT + "/changeUserPassword", user);
   }
@@ -40,25 +45,40 @@ export class AuthService implements CanActivate{
     return this.http.get(REST_SERVICE_ROOT + "/isFirstLogin");
   }
 
-  createFirstAccount(user:User): Observable<Object> {
+  createFirstAccount(user: User): Observable<Object> {
     return this.http.post(REST_SERVICE_ROOT + "/addInitialUser", user);
+  }
+
+  checkServerIsAuthenticated(): void {
+
+    if (localStorage.getItem('authenticated')) {
+      this.http.get(REST_SERVICE_ROOT + "/isAuthenticated").subscribe(data => {
+
+        this.isAuthenticated = data["success"];
+        if (!this.isAuthenticated) {
+          this.router.navigateByUrl('/pages/login');
+        }
+      },
+        error => {
+          this.isAuthenticated = false;
+          this.router.navigateByUrl('/pages/login');
+        });
+    }
   }
 
   canActivate(): boolean {
     /*
-    this.http.get(REST_SERVICE_ROOT + "/isAuthenticated").subscribe(data => {
-      console.log("is authenticated" + data["success"]);
-      this.isAuthenticated = data["success"];
-    });
+    
     */
-    if (localStorage.getItem('authenticated')) { 
+
+    if (localStorage.getItem('authenticated') && this.isAuthenticated) {
       return true;
     }
     else {
       this.router.navigateByUrl('/pages/login');
       return false;
     }
-    
+
   }
 
 }
