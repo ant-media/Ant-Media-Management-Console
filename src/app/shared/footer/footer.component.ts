5c8239e3e7c16fd928abcd48faaacb0114bb475c
mekya <ahmetmermerkaya@gmail.com>
2017-11-17 16:07:35 +0300

    Add language switcher to footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit, ElementRef, LOCALE_ID, Inject, Injectable } from '@angular/core';
+import { Locale } from 'app/locale/locale';
 
 @Component({
     moduleId: module.id,
@@ -8,4 +10,17 @@ import { Component } from '@angular/core';
 
 export class FooterComponent{
     test : Date = new Date();
+
+    public target_language:string;
+
+    constructor(@Inject(LOCALE_ID) locale: string) {
+        
+        if (locale == "en-US") {
+            this.target_language = Locale.getLocaleInterface().turkish_language;
+        }
+        else if (locale == "tr") {
+           this.target_language = Locale.getLocaleInterface().english_language;
+        }
+      
+    }
 }
