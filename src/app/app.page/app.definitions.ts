fc1a7d2eb295d102c507b2c460e0a67060fdead6
burak <burak@antmedia.io>
2025-08-03 12:43:54 +0300

    add av1 recording checks

--- src/app/app.page/app.definitions.ts
+++ src/app/app.page/app.definitions.ts
@@ -222,6 +222,7 @@ export class AppSettings {
                 public remoteAllowedCIDR: string,
                 public h264Enabled: boolean,
                 public vp8Enabled: boolean,
+                public av1Enabled: boolean,
                 public dataChannelEnabled: boolean,
                 public dataChannelPlayerDistribution: string,
                 public listenerHookURL: string,
