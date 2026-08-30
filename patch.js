62f8d416918e6bf5a9c511f049055750cd4cdc60
burak <burak@antmedia.io>
2026-08-30 15:42:01 +0300

    Fix Karma CI test middleware error

--- patch.js
+++ patch.js
@@ -1,13 +1,29 @@
 const fs = require('fs');
-const f = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const browserConfigFile = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+const karmaPluginFile = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/plugins/karma.js';
 
-fs.readFile(f, 'utf8', function (err,data) {
+fs.readFile(browserConfigFile, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   var result = data.replace(/node: false/g, 'node: {crypto: true, stream: true}');
 
-  fs.writeFile(f, result, 'utf8', function (err) {
+  fs.writeFile(browserConfigFile, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
+
+fs.readFile(karmaPluginFile, 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  var result = data.replace(
+    'const webpackReq = { ...req, url: webpackUrl };',
+    'const webpackReq = { ...req, url: webpackUrl, headers: req.headers || {} };'
+  );
+
+  fs.writeFile(karmaPluginFile, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
